ef9fafe00169425d5c5936458c8484cbfc356f5b
Usama Bin Nadeem <usamabinnadeem10@gmail.com>
2021-11-04 20:12:14 +0500

    new api data format for daybook and all stock

--- src/app/views/Daybook/Daybook.js
+++ src/app/views/Daybook/Daybook.js
@@ -29,13 +29,47 @@ const Daybook = () => {
 
   const [loading, setLoading] = useState(true);
 
+  const findIndexAndAppendData = (array, data) => {
+    let newAccountsData = [...array];
+    data.forEach((element) => {
+      let idx = newAccountsData.findIndex(
+        (account) => account.account_type === element.account_type__name
+      );
+      if (idx >= 0) {
+        let prevBalance = newAccountsData[idx].balance;
+        newAccountsData[idx] = {
+          ...newAccountsData[idx],
+          balance:
+            element.nature === "C"
+              ? prevBalance + element.amount
+              : prevBalance - element.amount,
+        };
+      } else {
+        newAccountsData.push({
+          account_type: element.account_type__name,
+          balance: element.amount,
+        });
+      }
+    });
+    return newAccountsData;
+  };
+
+  const formatAccountsData = (ledgersData, expensesData) => {
+    let newAccountsData = [];
+    newAccountsData = findIndexAndAppendData(newAccountsData, ledgersData);
+    newAccountsData = findIndexAndAppendData(newAccountsData, expensesData);
+    return newAccountsData;
+  };
+
   const fetchDaybook = () => {
     setLoading(true);
     instance.get(ESSENTIAL_URLS.DAY_BOOK).then((res) => {
       setTransactions(res.data.transactions);
       setLedgers(res.data.ledgers);
       setExpenses(res.data.expenses);
-      setAccounts(res.data.accounts);
+      setAccounts(
+        formatAccountsData(res.data.balance_ledgers, res.data.balance_expenses)
+      );
       setLoading(false);
     });
   };
--- src/app/views/ViewAllStock/ViewAllStock.js
+++ src/app/views/ViewAllStock/ViewAllStock.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
+import { useSelector } from "react-redux";
+
 import CustomTable from "../../components/CustomTable/CustomTable";
 import CustomLoader from "../../components/CustomLoader/CustomLoader";
 
@@ -18,18 +20,36 @@ const ViewAllStock = () => {
   const [stockData, setStockData] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const products = useSelector((state) => state.essentials.products);
+
+  const formatStockData = (data) => {
+    let newStockData = [];
+    for (let key in data) {
+      for (let head in products) {
+        products[head].forEach((element) => {
+          if (element.value === key) {
+            let qIn = data[key].C || 0;
+            let qOut = data[key].D || 0;
+            newStockData.push({
+              id: key,
+              product: `${element.head_name} ${element.label}`,
+              quantity_in: qIn,
+              quantity_out: qOut,
+              quantity: qIn - qOut,
+            });
+            return;
+          }
+        });
+      }
+    }
+    return newStockData;
+  };
+
   useEffect(() => {
     instance
       .get(TRANSACTION_URLS.ALL_STOCK)
       .then((res) => {
-        setStockData(
-          res.data.map((element, index) => {
-            return {
-              ...element,
-              id: index,
-            };
-          })
-        );
+        setStockData(formatStockData(res.data));
         setLoading(false);
       })
       .catch((error) => {
