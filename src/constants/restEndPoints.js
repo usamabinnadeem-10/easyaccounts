b687d62039eba91f5f54968c633b8d891ec33ebe
Usama Bin Nadeem <usamabinnadeem10@gmail.com>
2021-11-05 20:04:30 +0500

    Prior to this change, This change

--- src/constants/restEndPoints.js
+++ src/constants/restEndPoints.js
@@ -1,4 +1,5 @@
-export const BASE = "https://usamabinnadeem10.pythonanywhere.com/";
+// export const BASE = "https://usamabinnadeem10.pythonanywhere.com/";
+export const BASE = "http://127.0.0.1:8000/";
 
 const ESSENTIALS = "essentials/";
 const CUSTOMER = "C";
