3cde405b649a7efd90804646fffde77a1f6b0d49
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-04-08 22:15:51 +0500

    remove transfer from all stock

--- src/app/views/ViewAllStock/ViewAllStock.js
+++ src/app/views/ViewAllStock/ViewAllStock.js
@@ -9,8 +9,6 @@ import { useSelector } from 'react-redux';
 
 import { useReactToPrint } from 'react-to-print';
 
-import Select from 'react-select';
-
 import CustomFilters from '../../containers/CustomFilters/CustomFilters';
 import CustomTable from '../../components/CustomTable/CustomTable';
 import CustomLoader from '../../components/CustomLoader/CustomLoader';
@@ -18,19 +16,13 @@ import Empty from '../../components/Empty/Empty';
 import Heading from '../../components/Heading';
 
 import { Button } from '@mui/material';
-import { Modal } from '@mui/material';
-import { Typography } from '@mui/material';
-import { TextField } from '@mui/material';
 
-import { StyledPaper } from './styled';
-import { useStyles, selectStyles } from './styles';
+import { useStyles } from './styles';
 import { getColumns } from './constants';
 import { formatStockData, getFilters } from './utils';
 import { ESSENTIAL_URLS } from '../../../constants/restEndPoints';
 
 import { getAllStock } from '../../../store/transactions/actions';
-import instance from '../../../utils/axiosApi';
-import { TRANSACTION_URLS } from '../../../constants/restEndPoints';
 
 import { withSnackbar } from '../../hoc/withSnackbar';
 
@@ -54,19 +46,8 @@ const ViewAllStock = (props) => {
 
   const [stockData, setStockData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentStock, setCurrentStock] = useState(null);
-  const [transferQuantity, setTransferQuantity] = useState(null);
-  const [toWarehouse, setToWarehouse] = useState(null);
 
-  const handleTransferClick = (rowId) => {
-    if (allStock.length > 0) {
-      setCurrentStock(allStock.filter((stock) => stock.id === rowId)[0]);
-      setIsModalOpen(true);
-    }
-  };
-
-  const COLUMNS = useMemo(() => getColumns(handleTransferClick), [allStock]);
+  const COLUMNS = useMemo(() => getColumns(), [allStock]);
 
   useEffect(() => {
     if (stock.shouldFetchStock) {
@@ -87,27 +68,6 @@ const ViewAllStock = (props) => {
     }
   }, [allStock]);
 
-  const makeTransfer = () => {
-    instance
-      .post(TRANSACTION_URLS.TRANSFER_STOCK, {
-        id: currentStock.id,
-        to_warehouse: toWarehouse.value,
-        transfer_quantity: transferQuantity,
-      })
-      .then((response) => {
-        setIsModalOpen(false);
-        setCurrentStock(null);
-        setTransferQuantity(null);
-        props.showSuccessSnackbar('Stock transferred');
-        dispatch(getAllStock());
-      })
-      .catch((error) => {
-        props.showErrorSnackbar(
-          error?.response?.data?.detail || 'Oops, something went wrong'
-        );
-      });
-  };
-
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
@@ -118,56 +78,6 @@ const ViewAllStock = (props) => {
 
   return (
     <>
-      {isModalOpen && (
-        <Modal
-          paper='true'
-          open={isModalOpen}
-          onClose={() => setIsModalOpen(false)}>
-          <StyledPaper>
-            <Typography sx={{ mb: 2 }} variant='h5'>
-              Transfer Stock
-            </Typography>
-            <Typography variant='body1'>
-              Product: {props.products[currentStock.product].label}
-            </Typography>
-            <Typography variant='body1'>
-              From Warehouse: {props.warehouses[currentStock.warehouse].label}
-            </Typography>
-            <Typography variant='body1'>
-              Gazaana: {currentStock.yards_per_piece}
-            </Typography>
-            <Typography sx={{ mb: 2 }} variant='body1'>
-              Stock: {currentStock.stock_quantity}
-            </Typography>
-            <Select
-              styles={selectStyles()}
-              value={toWarehouse}
-              options={warehouses}
-              placeholder='Transfer to Warehouse'
-              onChange={(value) => setToWarehouse(value)}
-            />
-            <TextField
-              label='Transfer Quantity'
-              type='number'
-              sx={{ my: 2 }}
-              size='small'
-              value={transferQuantity}
-              helperText={`Quantity can not be greater than ${currentStock.stock_quantity}`}
-              error={transferQuantity > currentStock.stock_quantity}
-              InputProps={{
-                inputProps: {
-                  max: currentStock.stock_quantity,
-                  min: 1,
-                },
-              }}
-              onChange={(e) => setTransferQuantity(parseInt(e.target.value))}
-            />
-            <Button variant='contained' onClick={makeTransfer} fullWidth>
-              TRANSFER
-            </Button>
-          </StyledPaper>
-        </Modal>
-      )}
       <CustomFilters
         api={ESSENTIAL_URLS.ALL_STOCK}
         filters={filters}
--- src/app/views/ViewAllStock/constants.js
+++ src/app/views/ViewAllStock/constants.js
@@ -1,44 +1,24 @@
-import { IconButton } from "@mui/material";
-
-import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
-
-export const getColumns = (handleClick) => {
+export const getColumns = () => {
   return [
     {
-      accessor: "product",
-      Header: "Product",
+      accessor: 'product',
+      Header: 'Product',
     },
     {
-      accessor: "warehouse",
-      Header: "Warehouse",
+      accessor: 'warehouse',
+      Header: 'Warehouse',
     },
     {
-      accessor: "yards_per_piece",
-      Header: "Gazaana",
+      accessor: 'yards_per_piece',
+      Header: 'Gazaana',
     },
     {
-      accessor: "stock_quantity",
-      Header: "Stock",
+      accessor: 'stock_quantity',
+      Header: 'Stock',
     },
     {
-      accessor: "total_gazaana",
-      Header: "Total Gazaana",
-    },
-    {
-      accessor: "transfer_button",
-      Header: "Transfer",
-      hideInPrint: true,
-      Cell: (row) => {
-        if (row.row.id) {
-          return (
-            <IconButton onClick={() => handleClick(row.row.id)}>
-              <CompareArrowsIcon />
-            </IconButton>
-          );
-        } else {
-          return <div></div>;
-        }
-      },
+      accessor: 'total_gazaana',
+      Header: 'Total Gazaana',
     },
   ];
 };
