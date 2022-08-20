64bc5d6f47c8efc8c682bed38b12f105f1d9d90c
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-08-20 23:57:24 +0500

    fix NaN issue in product performance table

--- src/app/views/ProductPerformance/constants.js
+++ src/app/views/ProductPerformance/constants.js
@@ -6,26 +6,36 @@ export const COLUMNS = [
   {
     accessor: 'quantity_sold',
     Header: 'Thaan sold',
-    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
+    Cell: (row) => (
+      <div>{row.value ? parseFloat(row.value).toFixed(2) : '---'}</div>
+    ),
   },
   {
     accessor: 'average_rate',
     Header: 'Avg rate',
-    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
+    Cell: (row) => (
+      <div>{row.value ? parseFloat(row.value).toFixed(2) : '---'}</div>
+    ),
   },
   {
     accessor: 'minimum_rate',
     Header: 'Min rate',
-    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
+    Cell: (row) => (
+      <div>{row.value ? parseFloat(row.value).toFixed(2) : '---'}</div>
+    ),
   },
   {
     accessor: 'maximum_rate',
     Header: 'Max rate',
-    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
+    Cell: (row) => (
+      <div>{row.value ? parseFloat(row.value).toFixed(2) : '---'}</div>
+    ),
   },
   {
     accessor: 'number_of_times_sold',
     Header: 'Number of bills',
-    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
+    Cell: (row) => (
+      <div>{row.value ? parseFloat(row.value).toFixed(2) : '---'}</div>
+    ),
   },
 ];
