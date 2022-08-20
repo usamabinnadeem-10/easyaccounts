4ba31556e85c128181f3a6acc62ea818f922bbe4
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-08-20 23:49:17 +0500

    fix decimal points in product performance

--- src/app/views/ProductPerformance/constants.js
+++ src/app/views/ProductPerformance/constants.js
@@ -1,26 +1,31 @@
 export const COLUMNS = [
   {
-    accessor: "product__name",
-    Header: "Product",
+    accessor: 'product__name',
+    Header: 'Product',
   },
   {
-    accessor: "quantity_sold",
-    Header: "Thaan sold",
+    accessor: 'quantity_sold',
+    Header: 'Thaan sold',
+    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
   },
   {
-    accessor: "average_rate",
-    Header: "Avg rate",
+    accessor: 'average_rate',
+    Header: 'Avg rate',
+    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
   },
   {
-    accessor: "minimum_rate",
-    Header: "Min rate",
+    accessor: 'minimum_rate',
+    Header: 'Min rate',
+    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
   },
   {
-    accessor: "maximum_rate",
-    Header: "Max rate",
+    accessor: 'maximum_rate',
+    Header: 'Max rate',
+    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
   },
   {
-    accessor: "number_of_times_sold",
-    Header: "Number of bills",
+    accessor: 'number_of_times_sold',
+    Header: 'Number of bills',
+    Cell: (row) => <div>{parseFloat(row.value).toFixed(2)}</div>,
   },
 ];
