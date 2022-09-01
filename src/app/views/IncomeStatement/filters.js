3f13ff60a22f8019f2ab08682ed3e80710afd3c4
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-09-01 23:13:10 +0500

    fix a bug in income statement filters

--- src/app/views/IncomeStatement/filters.js
+++ src/app/views/IncomeStatement/filters.js
@@ -5,10 +5,12 @@ export const FILTERS = [
     qp: 'date__gte',
     type: FIELDS.DATE,
     placeholder: 'Date (more than)',
+    variant: 'start',
   },
   {
     qp: 'date__lte',
     type: FIELDS.DATE,
     placeholder: 'Date (less than)',
+    variant: 'end',
   },
 ];
