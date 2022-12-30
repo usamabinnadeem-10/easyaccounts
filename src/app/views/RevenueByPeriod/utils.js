4cb3b7586a679a842826ca8dadd4c5bbf7d0cb26
Usama Bin Nadeem <32700508+usamabinnadeem-10@users.noreply.github.com>
2022-12-31 02:06:44 +0500

    fix

--- src/app/views/RevenueByPeriod/utils.js
+++ src/app/views/RevenueByPeriod/utils.js
@@ -1,8 +1,22 @@
 import { v4 } from 'uuid';
 import moment from 'moment';
+
+const mergeArrays = (arr1, arr2) => {
+  let merged = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    merged.push({
+      ...arr1[i],
+      ...arr2[i],
+    });
+  }
+  return merged;
+};
+
 export const getTableData = (data) => {
-  const { revenue, period } = data;
-  const sorted = revenue.sort(function (left, right) {
+  const { revenue, period, discounts } = data;
+  const merged = mergeArrays(revenue, discounts);
+  const sorted = merged.sort(function (left, right) {
     return moment.utc(left.period).diff(moment.utc(right.period));
   });
   return sorted.map((r) => ({
